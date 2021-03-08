280031ed1c7d2cab21936c9f9b2a642f8a3f5910
mohamedqabbari <mqabbari77@gmail.com>
2021-03-08 17:46:32 +0200

    Math Functions in PHP

--- index.php
+++ index.php
@@ -26,6 +26,13 @@
           echo "<p>The " . $key . " costs $" . $value ."<p>";
         }
       };
+
+      function tax_calc($amount,$tax){
+        $calculate_tax = $amount*$tax;
+        $amount = round($amount+$calculate_tax,2);
+        return $amount;
+      }
+      echo tax_calc(750,0.223);
     ?>
   </body>
 </html>
