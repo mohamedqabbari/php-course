ea6ce3093c658901c7eda4e401c37eeb58297edd
mohamedqabbari <mqabbari77@gmail.com>
2021-03-08 14:31:10 +0200

    Building Array Variables with PHP

--- index.php
+++ index.php
@@ -10,6 +10,13 @@
 
       echo "<h1>Welcome to ".$name."!</h1>";
       echo "<h2>You have $".$credit." in your wallet.</h2>";
+
+      $products['computer'] = 750;
+      $products['car'] = 15000;
+      $products['iphone'] = 1000;
+      $products['toaster'] = 75;
+
+      echo "<p>A car costs ". $products['car'] ."</p>";
     ?>
   </body>
 </html>
